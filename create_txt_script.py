34d968f3feac6a08c7cf09a867257ff77ed13907
joelmccl <joelmccl@iastate.edu>
2024-11-04 15:10:02 -0600

    testing workflow

--- create_txt_script.py
+++ create_txt_script.py
@@ -1,5 +1,6 @@
 numbers = [5, 10, 20, 40, 80] #numbers
 total = sum(numbers) #sum
+
 # Create and write the result to a text file
 with open('result.txt', 'w') as file:
     file.write(f'The total sum is: {total}\n')
